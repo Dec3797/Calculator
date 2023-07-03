0b038b1523fa3c787342d612a5fca33d95e0a787
Dec <declan_meek@yahoo.com>
2023-07-03 14:53:44 +0100

    Simplify operator functions

--- script.js
+++ script.js
@@ -9,30 +9,18 @@ let displayValue = '';
 
 function operate (number1, operator, number2){
     if (operator == '+'){
-        add(number1, number2);
+        return number1 + number2;
     } else if (operator == '-'){
-        subtract(number1, number2);
+        return number1 - number2;
     } else if (operator == 'x'){
-        multiply(number1, number2);
+        return number1 * number2;
     } else if (operator == '%'){
-        divide(number1, number2)
+        if(number1 == 0 && number2 == 0){
+            return 'Please don\'t try and break me :('
+        } else {
+            return number1 / number2;
+        }
     }
-}
-
-function add(num1, num2){
-    return num1 + num2
-}
-
-function subtract(num1, num2){
-    return num1 - num2
-}
-
-function multiply(num1, num2){
-    return num1 * num2
-}
-
-function divide(num1, num2){
-    return num1 / num2
 }
 
 numBtn.forEach((button) => {
