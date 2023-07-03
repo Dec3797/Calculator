ae5c0e7e0c789136165b7008c6d2923655129e2b
Dec <declan_meek@yahoo.com>
2023-07-03 11:40:56 +0100

    Create operator function and caluculation variables

--- script.js
+++ script.js
@@ -1,3 +1,19 @@
+let number1 = '';
+let number2 = '';
+let operator = '';
+
+function operate (number1, operator, number2){
+    if (operator == '+'){
+        add(number1, number2);
+    } else if (operator == '-'){
+        subtract(number1, number2);
+    } else if (operator == 'x'){
+        multiply(number1, number2);
+    } else if (operator == '%'){
+        divide(number1, number2)
+    }
+}
+
 function add(num1, num2){
     return num1 + num2
 }
