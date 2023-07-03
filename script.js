716ca62d3ee45d26550c640af750e4f37286787e
Dec <declan_meek@yahoo.com>
2023-07-03 15:01:10 +0100

    Update click event to include all buttons

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 let number1 = '';
 let number2 = '';
 let operator = '';
-const numBtn = document.querySelectorAll('button');
+const btn = document.querySelectorAll('button');
 const display = document.querySelector('#display');
 const content = document.createElement('p');
 content.classList.add('content');
@@ -23,21 +23,10 @@ function operate (number1, operator, number2){
     }
 }
 
-numBtn.forEach((button) => {
-    button.addEventListener('click', () => {
-        if (button.id == '1' ||
-        button.id == '2' ||
-        button.id == '3' ||
-        button.id == '4' ||
-        button.id == '5' ||
-        button.id == '6' ||
-        button.id == '7' ||
-        button.id == '8' ||
-        button.id == '9' ||
-        button.id == '0'){
-            content.textContent = button.id
-            display.appendChild(content)
-            displayValue = button.id
-        }
+btn.forEach((button) => {
+    button.addEventListener('click', () => {        
+        content.textContent = button.id
+        display.appendChild(content)
+        displayValue = button.id
     })
 })
