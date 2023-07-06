2fa51dbd58dd2d298aea7010c603918846690e87
Dec <declan_meek@yahoo.com>
2023-07-06 12:17:21 +0100

    Rework operator function

--- script.js
+++ script.js
@@ -1,32 +1,27 @@
-let number1 = '';
-let number2 = '';
-let operator = '';
-const btn = document.querySelectorAll('button');
-const display = document.querySelector('#display');
-const content = document.createElement('p');
-content.classList.add('content');
-let displayValue = '';
-
-function operate (number1, operator, number2){
-    if (operator == '+'){
-        return number1 + number2;
-    } else if (operator == '-'){
-        return number1 - number2;
-    } else if (operator == 'x'){
-        return number1 * number2;
-    } else if (operator == '%'){
-        if(number1 == 0 && number2 == 0){
-            return 'Please don\'t try and break me :('
-        } else {
-            return number1 / number2;
-        }
-    }
+function add(number1, number2){
+    return number1 + number2
 }
 
-btn.forEach((button) => {
-    button.addEventListener('click', () => {        
-        content.textContent = button.id
-        display.appendChild(content)
-        displayValue = button.id
-    })
-})
+function subtract(number1, number2){
+    return number1 - number2
+}
+
+function multiply(number1, number2){
+    return number1 * number2
+}
+
+function divide(number1, number2){
+    return number1 / number2
+}
+
+function operate(number1, operand, number2){
+    if (operand == '+'){
+        return add(number1, number2)
+    } else if(operand == '-'){
+        return subtract(number1, number2)
+    } else if(operand == '*'){
+        return multiply(number1, number2)
+    } else if(operand == '/'){
+        return divide(number1, number2)
+    }
+}
